43bc0c595dd6f279fae6c991e9a02ebb90ce9cd4
David J. Yu <87633683+ddjnw1yu@users.noreply.github.com>
2024-06-11 14:18:52 +1200

    Export

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import DrawToolbar from "./DrawToolbar/DrawToolbar.vue";
+import HelpModeDialog from "./HelpModeDialog/HelpModeDialog.vue";
+import Tooltip from "./Tooltip/Tooltip.vue";
+import TreeControls from "./TreeControls/TreeControls.vue";
+
 const app = createApp(App);
+
+app.component("DrawToolbar", DrawToolbar);
+app.component("HelpModeDialog", HelpModeDialog);
+app.component("Tooltip", Tooltip);
+app.component("TreeControls", TreeControls);
+
 app.mount("#app");
