f4c6decee849e4831eb59f13e80e36c7e4584696
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-10-14 17:59:10 +1300

    Add connectivity graph component

---
+++ src/components/ConnectivityGraph/ConnectivityGraph.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="connectivity-graph">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ConnectivityGraph',
+  props: {
+    /**
+     * Entity to load its connectivity graph.
+     */
+    entry: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .connectivity-graph {
+
+  }
+</style>
