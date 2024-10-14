f6bdd0761085cbe2b0c72c9917664927d6ffef2e
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-10-15 11:24:24 +1300

    Show connectivity graph from iframe

--- src/components/ConnectivityGraph/ConnectivityGraph.vue
+++ src/components/ConnectivityGraph/ConnectivityGraph.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="connectivity-graph">
-
+    <iframe
+      ref="connectivityGraphFrame"
+      src="https://mapcore-demo.org/curation/flatmap/connectivity-graph/"
+      width="100%"
+      height="100%"
+    />
   </div>
 </template>
 
@@ -16,11 +21,32 @@ export default {
       default: '',
     },
   },
+  mounted() {
+    this.loadConnectivityGraph();
+  },
+  methods: {
+    loadConnectivityGraph: function () {
+      console.log('Connnectivity Graph Component', this.entry)
+      const iframeRef = this.$refs.connectivityGraphFrame;
+      if (iframeRef) {
+        iframeRef.onload = function() {
+          console.log('iframeRef.contentWindow', iframeRef.contentWindow)
+          iframeRef.contentWindow.postMessage({ backgroundColor: 'lightblue' }, iframeRef.src);
+        };
+      }
+    },
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   .connectivity-graph {
+    width: 100%;
+    height: 100%;
 
+    iframe {
+      width: 100%;
+      border: 0 none;
+    }
   }
 </style>
