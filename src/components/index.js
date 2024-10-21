df2351ddd86f803f22fb05cd05c7bcea394e49ba
alan-wu <alan.wu@auckland.ac.nz>
2024-10-21 16:45:15 +1300

    Add annotation popup to export list.

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
+import AnnotationPopup from "./Tooltip/AnnotationPopup.vue";
+import CopyToClipboard from "./CopyToClipboard/CopyToClipboard.vue";
 import DrawToolbar from "./DrawToolbar/DrawToolbar.vue";
 import HelpModeDialog from "./HelpModeDialog/HelpModeDialog.vue";
 import Tooltip from "./Tooltip/Tooltip.vue";
 import TreeControls from "./TreeControls/TreeControls.vue";
-import CopyToClipboard from "./CopyToClipboard/CopyToClipboard.vue";
 
-export { DrawToolbar, HelpModeDialog, Tooltip, TreeControls, CopyToClipboard };
+export { AnnotationPopup, CopyToClipboard, DrawToolbar, HelpModeDialog, Tooltip, TreeControls };
