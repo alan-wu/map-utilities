91f5bdcbf5a844e5b66f15db3fa166ac1d2969cd
alan-wu <alan.wu@auckland.ac.nz>
2024-10-25 10:35:43 +1300

    Add create tooltip content component.

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,9 @@
 import AnnotationPopup from "./Tooltip/AnnotationPopup.vue";
+import CreateTooltipContent from "./Tooltip/CreateTooltipContent.vue";
 import CopyToClipboard from "./CopyToClipboard/CopyToClipboard.vue";
 import DrawToolbar from "./DrawToolbar/DrawToolbar.vue";
 import HelpModeDialog from "./HelpModeDialog/HelpModeDialog.vue";
 import Tooltip from "./Tooltip/Tooltip.vue";
 import TreeControls from "./TreeControls/TreeControls.vue";
 
-export { AnnotationPopup, CopyToClipboard, DrawToolbar, HelpModeDialog, Tooltip, TreeControls };
+export { AnnotationPopup, CreateTooltipContent, CopyToClipboard, DrawToolbar, HelpModeDialog, Tooltip, TreeControls };
