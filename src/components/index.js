a4625846e588493b0fee095486f53d862fe73f01
alan-wu <alan.wu@auckland.ac.nz>
2024-11-14 15:19:10 +1300

    Fix an error.

--- src/components/index.js
+++ src/components/index.js
@@ -1,12 +1,11 @@
 import AnnotationPopup from "./Tooltip/AnnotationPopup.vue";
 import CreateTooltipContent from "./Tooltip/CreateTooltipContent.vue";
+import ConnectivityGraph from "./ConnectivityGraph/ConnectivityGraph.vue";
 import CopyToClipboard from "./CopyToClipboard/CopyToClipboard.vue";
 import DrawToolbar from "./DrawToolbar/DrawToolbar.vue";
 import HelpModeDialog from "./HelpModeDialog/HelpModeDialog.vue";
 import Tooltip from "./Tooltip/Tooltip.vue";
 import TreeControls from "./TreeControls/TreeControls.vue";
-import CopyToClipboard from "./CopyToClipboard/CopyToClipboard.vue";
-import ConnectivityGraph from "./ConnectivityGraph/ConnectivityGraph.vue";
 
 export {
   AnnotationPopup,
