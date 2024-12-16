4d5967a1d31f675efb966387d49620a756396f89
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-12-16 16:59:21 +1300

    Rewrite external-resource-card component

--- src/components/Tooltip/ExternalResourceCard.vue
+++ src/components/Tooltip/ExternalResourceCard.vue
@@ -1,34 +1,54 @@
 <template>
-  <div class="resource-container" v-if="resources.length">
+  <div class="resource-container">
     <div class="attribute-title-container">
       <div class="attribute-title">References</div>
     </div>
-    <div class="resource" v-for="resource in transformedResources" :key="resource.dataId">
+    <div class="citation-tabs">
       <el-button
         link
-        class="button"
-        id="open-pubmed-button"
-        :icon="getIconByType(resource.id)"
-        @click="openUrl(resource.url)"
+        v-for="citationOption of citationOptions"
+        :key="citationOption.value"
+        :type="citationType === citationOption.value ? 'primary' : ''"
+        @click="onCitationFormatChange(citationOption.value)"
       >
-        {{ resource.title || resource.url }}
+        {{ citationOption.label }}
       </el-button>
     </div>
-
+    <ul class="citation-list">
+      <li
+        v-for="url of urls"
+        :key="url.id"
+        :class="{'loading': url.citation && url.citation[citationType] === ''}"
+      >
+        <span v-if="url.citation">
+          {{ url.citation[citationType] }}
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-/* eslint-disable no-alert, no-console */
-import { shallowRef } from "vue";
-import {
-  Notebook as ElIconNotebook,
-  Reading,
-  Memo,
-} from "@element-plus/icons-vue";
-
-import EventBus from "../EventBus.js";
-import { xmlToJSON } from "../utilities.js";
+const CROSSCITE_API_HOST = 'https://citation.crosscite.org';
+const CITATION_OPTIONS = [
+  {
+    label: 'APA',
+    value: 'apa',
+  },
+  {
+    label: 'Chicago',
+    value: 'chicago-note-bibliography',
+  },
+  {
+    label: 'IEEE',
+    value: 'ieee',
+  },
+  {
+    label: 'Bibtex',
+    value: 'bibtex',
+  },
+];
+const CITATION_DEFAULT = 'apa';
 
 export default {
   name: "ExternalResourceCard",
@@ -40,119 +60,220 @@ export default {
   },
   data: function () {
     return {
-      pubmeds: [],
-      pubmedIds: [],
-      transformedResources: [],
-      ElIconNotebook: shallowRef(ElIconNotebook),
-    };
-  },
-  mounted: function () {
-    this.transformResources(this.resources);
-  },
-  watch: {
-    resources: async function (_resources) {
-      this.transformResources(_resources);
+      urls: [],
+      citationOptions: CITATION_OPTIONS,
+      citationType: CITATION_DEFAULT,
     }
   },
+  watch: {
+    resources: function (_resources) {
+      this.urls = this.getURLsForPubMed(_resources);
+      this.urls = this.formatURLs([..._resources]);
+    }
+  },
+  mounted: function () {
+    this.urls = this.formatURLs([...this.resources]);
+    this.getCitationText(CITATION_DEFAULT);
+  },
   methods: {
-    transformResources: async function (_resources) {
-      this.transformedResources = [];
+    formatURLs: function (urls) {
+      const nonPubMedURLs = this.extractNonPubMedURLs(urls);
+      const pubMedURLs = urls.filter(url => !nonPubMedURLs.includes(url));
+      const formattedURLs = pubMedURLs.map((url) =>
+        (typeof url === 'object') ?
+        this.extractPublicationIdFromURLString(url[0]) :
+        this.extractPublicationIdFromURLString(url)
+      );
+      return formattedURLs;
+    },
+    extractNonPubMedURLs: function (urls) {
+      const extractedURLs = [];
+      const names = this.getPubMedDomains();
 
-      if (_resources.length) {
-        for (const resource of _resources) {
-          try {
-            if (resource.id === 'pubmed') {
-              const { title, abstract } = await this.fetchArticle(resource.dataId);
-
-              this.transformedResources.push({
-                ...resource,
-                title,
-                abstract,
-              });
-            } else {
-              let title = '';
-              const url = resource.url;
-              const splitURLs = url.split('/');
-              const index = splitURLs.indexOf('books') + 1;
-              const id = splitURLs[index];
-
-              if (resource.id === 'openlib') {
-                const data = await this.fetchBook(id);
-                title = data.title;
-              } else {
-                title = url;
-              }
-
-              this.transformedResources.push({
-                ...resource,
-                title,
-              });
-            }
-          } catch (error) {
-            console.error(`Error fetching data for id ${resource.dataId}:`, error);
+      urls.forEach((url) => {
+        let count = 0;
+        names.forEach((name) => {
+          if (url.includes(name)) {
+            count++;
           }
+        });
+        if (!count) {
+          extractedURLs.push(url);
         }
-      }
+      });
+
+      return extractedURLs;
     },
-    capitalise: function (string) {
-      return string.charAt(0).toUpperCase() + string.slice(1);
-    },
-    openUrl: function (url) {
-      EventBus.emit("open-pubmed-url", url);
-      window.open(url, "_blank");
-    },
-    getIconByType: function (type) {
-      if (type === 'pubmed') {
-        return Reading;
-      } else if (type === 'openlib') {
-        return ElIconNotebook;
-      }
-      return Memo;
-    },
-    fetchBook: async function (id) {
-      const bookAPI = `https://openlibrary.org/books/${id}.json`;
-      const response = await fetch(bookAPI);
-      const data = await response.json();
-      return data;
-    },
-    fetchArticle: async function (id) {
-      try {
-        const eutilsFetchAPI = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id=${id}`;
-        const response = await this.fetchText(eutilsFetchAPI);
-        const responseJSON = xmlToJSON(response);
-        const article = responseJSON?.PubmedArticleSet?.PubmedArticle?.MedlineCitation?.Article;
-        const title = article?.ArticleTitle || '';
-        const abstract = article?.AbstractText || '';
-        return {title, abstract};
-      } catch (error) {
-        console.warn('Fetch article error.', error)
-        return {title: '', abstract: ''};
-      }
-    },
-    fetchText: async function (url, maxRetries = 3) {
-      for (let i = 0; i < maxRetries; i++) {
-        try {
-          const response = await fetch(url);
-          if (response.status === 429) {
-            const retryAfter = response.headers.get('Retry-After');
-            const waitTime = retryAfter ? parseInt(retryAfter) * 1000 : 1000 * Math.pow(2, i);
-            await new Promise(resolve => setTimeout(resolve, waitTime));
-            continue;
+    getURLsForPubMed: function (data) {
+      return new Promise((resolve) => {
+        const ids = data.map((id) =>
+          (typeof id === 'object') ?
+          this.extractPublicationIdFromURLString(id[0]) :
+          this.extractPublicationIdFromURLString(id)
+        )
+        this.convertPublicationIds(ids).then((pmids) => {
+          if (pmids.length > 0) {
+            const transformedIDs = [];
+            pmids.forEach(pmid => {
+              transformedIDs.push({
+                id: pmid,
+                link: this.pubmedSearchUrl(pmid),
+              })
+            })
+            resolve(transformedIDs)
           } else {
-            const text = await response.text();
-            return text;
+            resolve([])
           }
-        } catch (error) {
-          if (i === maxRetries - 1) return error;
-          // CORS retry
-          const waitTime = 1000 * Math.pow(2, i);
-          await new Promise(resolve => setTimeout(resolve, waitTime));
-          continue;
+        })
+      })
+    },
+    extractPublicationIdFromURLString: function (urlStr) {
+      if (!urlStr) return
+
+      const str = decodeURIComponent(urlStr)
+
+      let term = {id: '', type: '', citation: {}}
+
+      const names = this.getPubMedDomains()
+
+      names.forEach((name) => {
+        const lastIndex = str.lastIndexOf(name)
+        if (lastIndex !== -1) {
+          term.id = str.slice(lastIndex + name.length)
+          if (name === 'doi.org/') {
+            term.type = "doi"
+          } else if (name === 'pmc/articles/') {
+            term.type = "pmc"
+          } else {
+            term.type = "pmid"
+          }
+        }
+      })
+
+      //Backward compatability with doi: and PMID:
+      if (term.id === '') {
+        if (urlStr.includes("doi:")) {
+          term.id = this.stripPMIDPrefix(urlStr)
+          term.type = "doi"
+        } else if (urlStr.includes("PMID:")) {
+          term.id = this.stripPMIDPrefix(urlStr)
+          term.type = "pmid"
         }
       }
+
+      if (term.id.endsWith('/')) {
+        term.id = term.id.slice(0, -1)
+      }
+
+      return term
+    },
+    getPubMedDomains: function () {
+      const names = [
+        'doi.org/',
+        'nih.gov/pubmed/',
+        'pmc/articles/',
+        'pubmed.ncbi.nlm.nih.gov/',
+      ]
+
+      return names;
+    },
+    convertPublicationIds: function (ids) {
+      return new Promise((resolve) => {
+        const pmids = []
+        const toBeConverted = []
+        ids.forEach((id) => {
+          if (id.type === "pmid") {
+            pmids.push(id.id)
+          } else if (id.type === "doi" || id.type === "pmc") {
+            toBeConverted.push(id.id)
+          }
+        })
+        this.getPMID(toBeConverted).then((idList) => {
+          pmids.push(...idList)
+          resolve(pmids)
+        })
+        .catch(() => {
+          resolve(pmids)
+        })
+      })
+    },
+    pubmedSearchUrl: function (ids) {
+      let url = 'https://pubmed.ncbi.nlm.nih.gov/?'
+      let params = new URLSearchParams()
+      params.append('term', ids)
+      return url + params.toString()
+    },
+    stripPMIDPrefix: function (pubmedId) {
+      return pubmedId.split(':')[1]
+    },
+    getPMID: function(idsList) {
+      return new Promise((resolve) => {
+        if (idsList.length > 0) {
+          //Muliple term search does not work well,
+          //DOIs term get splitted unexpectedly
+          //
+          const promises = []
+          const results = []
+          let wrapped = ''
+          idsList.forEach((id, i) => {
+            wrapped += i > 0 ? 'OR"' + id + '"' : '"' + id + '"'
+          })
+
+          const params = new URLSearchParams({
+            db: 'pubmed',
+            term: wrapped,
+            format: 'json'
+          })
+          const promise = fetch(`https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?${params}`, {
+            method: 'GET',
+          })
+          .then((response) => response.json())
+          .then((data) => {
+            const newIds = data.esearchresult ? data.esearchresult.idlist : []
+            results.push(...newIds)
+          })
+          promises.push(promise)
+
+          Promise.all(promises).then(() => {
+            resolve(results)
+          }).catch(() => {
+            resolve(results)
+          })
+        } else {
+          resolve([])
+        }
+      })
+    },
+    onCitationFormatChange: function(citationType) {
+      this.citationType = citationType;
+      this.getCitationText(citationType);
+    },
+    getCitationText: function(citationType) {
+      this.urls.forEach((url) => {
+        const {id, type} = url;
+        if (
+          !(url.citation && url.citation[citationType])
+          && type === 'doi'
+          && id
+        ) {
+          url.citation[citationType] = ''; // loading
+          const citationAPI = `${CROSSCITE_API_HOST}/format?doi=${id}&style=${citationType}&lang=en-US`;
+          fetch(citationAPI)
+            .then((response) => {
+              if (response.status !== 200) {
+                throw Error
+              }
+              return response.text();
+            }).then((text) => {
+              url.citation[citationType] = text;
+            }).catch((err) => {
+              console.error('Error', err)
+            });
+        }
+      });
     },
   },
-};
+}
 </script>
 
 <style lang="scss" scoped>
@@ -171,55 +292,38 @@ export default {
   text-transform: uppercase;
 }
 
-.attribute-content {
-  font-size: 14px;
-  font-weight: 400;
-}
-
-.el-link {
-  color: $app-primary-color;
-  text-decoration: none;
-  word-wrap: break-word;
-  &:hover,
-  &:focus {
-    color: $app-primary-color;
-  }
-}
-
-:deep(.el-carousel__button) {
-  background-color: $app-primary-color;
-}
-
-.attribute-title {
-  font-size: 16px;
-  font-weight: 600;
-  /* font-weight: bold; */
-  text-transform: uppercase;
-}
-
-.resource-container .button.is-link {
-  margin-left: 0px !important;
-  margin-top: 0px !important;
+.citation-list {
+  margin: 0;
+  margin-top: 0.5rem;
   padding: 0;
-  font-family: inherit;
-  font-size: 14px !important;
-  color: $app-primary-color;
-  max-width: 100%;
+  list-style: none;
+  line-height: 1.3;
 
-  :deep(span) {
-    display: inline;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    line-height: 1.2;
+  li {
+    margin: 0;
+    padding: 0.25rem 0.5rem;
+    border-radius: var(--el-border-radius-base);
+    background-color: var(--el-bg-color-page);
+
+    + li {
+      margin-top: 0.5rem;
+    }
+
+    &.loading {
+      padding: 0.5rem;
+    }
+  }
+}
+
+.citation-tabs {
+  .el-button {
+    &:hover {
+      color: var(--el-button-text-color);
+    }
   }
 
-  &:hover {
-    color: $app-primary-color;
-  }
-
-  & + .button {
-    margin-top: 10px !important;
-    color: $app-primary-color;
+  .el-button + .el-button {
+    margin-left: 0.25rem;
   }
 }
 </style>
